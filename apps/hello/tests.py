717be6387487e487650b919f64b69ad184c09f2d
igor <shevchenkcoigor@gmail.com>
2015-08-28 10:00:03 +0300

    contacts ticket:1 plan end task 28.08.2015 10:00 (tests fail)

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -6,14 +6,20 @@ from apps.hello.models import Contacts
 class ContactsTest(TestCase):
     def setUp(self):
         Contacts.objects.create(name='igor', surname='shevchenko')
-        Contacts.objects.create(name='igor', surname='shevchenko')
-        Contacts.objects.create(name='igor', surname='shevchenko')
+        Contacts.objects.create(name='igor1', surname='shevchenko1')
+        Contacts.objects.create(name='igor2', surname='shevchenko2')
 
     def test_contact_check_count_row(self):
         "check count row"
+        contacts = Contacts.objects.all()
+        self.assertEqual(contacts.count(), 2)
 
-    def test_contact_check_data(self):
+    def test_contact_check_data_name(self):
         "check data from database"
+        contact = Contacts.objects.get(name='igor')
+        self.assertEqual(contact.name, 'some name')
 
-    def test_create_record(self):
-        "check create record"
+    def test_contact_check_data_surname(self):
+        "check data from database"
+        contact = Contacts.objects.get(surname='shevchenko1')
+        self.assertEqual(contact.surname, 'some last name')
--- fortytwo_test_task/wsgi.py
+++ fortytwo_test_task/wsgi.py
@@ -8,7 +8,14 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
+from fortytwo_test_task.settings import common
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fortytwo_test_task.settings")
 
-from django.core.wsgi import get_wsgi_application
+activate_env = os.path.expanduser(os.path.join(common.BASE_DIR, "ENV/bin/activate_this.py"))
+execfile(activate_env, dict(__file__=activate_env))
+# Django server
 application = get_wsgi_application()
+
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
